d6ffe1e77945aed91b49aef10d029d3df2c13109
hatchwald <fatkhur139wec@gmail.com>
2021-01-22 21:12:38 +0700

    last update

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
     mix.sass('app.scss');
+    mix.browserify('main.js', 'public/javascripts/app.js');
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<head>
+    <title>Laravel</title>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Laravel 5</div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
